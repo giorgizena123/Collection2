258d43b0e20d8e2b1edac1ba3921c80a50086d0c
giorgizena123 <zenaishvili621@gmail.com>
2025-03-04 23:30:04 +0400

    Update WordCounter.java

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WordCounter {
+
+    public static void main(String[] args) {
+        String[] words = {"ვაშლი", "ბანანი", "ვაშლი", "ფორთოხალი", "ბანანი", "ვაშლი", "ატამი"};
+
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println(wordCount);
+    }
 }
